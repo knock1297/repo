991b25e5e221c03077422f5e3fab6b99ecaa4cd8
knock1297 <knock1297@gmail.com>
2018-12-11 04:38:09 +0900

    test

---
+++ Valet/src/main/java/kr/AJ/valet/ValetApplication.java
@@ -0,0 +1,12 @@
+package kr.AJ.valet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ValetApplication.class, args);
+	}
+}
